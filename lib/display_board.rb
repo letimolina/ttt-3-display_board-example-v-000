f8f928e2e72d3241e4e3e30c974f1b0eb669d21e
Leticia Molina <letimolina@gmail.com>
2018-04-19 03:38:58 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+#display_board method represents a cell as a string with 3 spaces
+
+def display_board
+  cell = "   "
+  print cell+"|"+cell+"|"+cell+"\n-----------\n"+cell+"|"+cell+"|"+cell+"\n-----------\n"+cell+"|"+cell+"|"+cell+"\n"
+
+end
